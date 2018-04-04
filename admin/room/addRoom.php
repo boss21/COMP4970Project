b9c8480d12c8ca31b7551a165094e1fa022e9c24
Timothy Boss <bosst@wit.edu>
2018-04-03 23:30:16 -0400

    update

--- admin/room/addRoom.php
+++ admin/room/addRoom.php
@@ -9,19 +9,39 @@ if (!isset($_SESSION["username"]) || empty($_SESSION["username"])) {
 // Include config file
 include '../../dbconfig.php';
 
+$room = "";
+$capacity = "";
+$room_err = "";
+$capacity_err = "";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $room = $_POST["room_name"];
-    $sql = "SELECT Room FROM Rooms WHERE Room = '$room'";
-    $result = mysqli_query($link, $sql);
-	if (mysqli_num_rows($result) != 0) {
-		echo "<script>alert('$room already exists.');window.location.href='addRoom.php';</script>";
-	} else {
-        $capacity = $_POST["capacity"];
-        $sqlAdd = "INSERT INTO Rooms (Room, Capacity) VALUES ('$room', '$capacity')";
-        if (mysqli_query($link, $sqlAdd)){
-            echo "<script>alert('$room was successfully added with a capacity of $capacity.');</script>";
-        }else{
-            echo "Oops! Something went wrong. Please try again later.";
+
+    // Check if Room Name is empty
+    if (empty(trim($_POST["room_name"]))) {
+        $room_err = "Please enter Room Name.";
+    } else {
+        $room = trim($_POST["room_name"]);
+    }
+    
+    // Check if Capacity is empty
+    if (empty(trim($_POST["capacity"]))) {
+        $capacity_err = "Please enter Capacity.";
+    } else {
+        $capacity = trim($_POST["capacity"]);
+    }
+    
+    if (empty($room_err) && empty($capacity_err)){
+        $sql = "SELECT Room FROM Rooms WHERE Room = '$room'";
+        $result = mysqli_query($link, $sql);
+        if (mysqli_num_rows($result) != 0) {
+            echo "<script>alert('$room already exists.');window.location.href='addRoom.php';</script>";
+        } else {
+            $sqlAdd = "INSERT INTO Rooms (Room, Capacity) VALUES ('$room', '$capacity')";
+            if (mysqli_query($link, $sqlAdd)){
+                echo "<script>alert('$room was successfully added with a capacity of $capacity.');</script>";
+            }else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
         }
     }
 }
@@ -54,13 +74,19 @@ mysqli_close($link);
             <div class="col-sm-4"></div>
             <div class="col-sm-4 text-center">
                 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                    <div class="form-group">
+                    <div class="form-group <?php echo (!empty($room_err)) ? "has-error" : ""; ?>">
                         <label>Room Name</label>
                         <input type="text" id="room_name" name="room_name" class="form-control" />
+                        <span class="help-block" style="color:red;">
+						    <?php echo $room_err; ?>
+						</span>
                     </div>
-                    <div class="form-group">
+                    <div class="form-group <?php echo (!empty($capacity_err)) ? "has-error" : ""; ?>">
                         <label>Capacity</label>
                         <input type="number" id="capacity" name="capacity" min="0" pattern="[0-9]" class="form-control" />
+                        <span class="help-block" style="color:red;">
+						    <?php echo $capacity_err; ?>
+						</span>
                     </div>
                     <input type="submit" name="submit" value="Add" class="btn btn-primary" />
                     <input type="reset" class="btn btn-default" />
