4b3037822b8d3383e0092b0a141f6117a44c3d55
Timothy Boss <bosst@wit.edu>
2018-04-04 00:19:59 -0400

    update

--- admin/user/addUser.php
+++ admin/user/addUser.php
@@ -9,18 +9,30 @@ if (!isset($_SESSION["username"]) || empty($_SESSION["username"])) {
 // Include config file
 include "../../dbconfig.php";
 
+$userID = "";
+$userID_err = "";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $userID = $_POST["userID"];
-    $sql = "SELECT UserID FROM Clients WHERE UserID = '$userID'";
-    $result = mysqli_query($link, $sql);
-    if (mysqli_num_rows($result) != 0) {
-        echo "<script>alert('$userID already exists.');window.location.href='addUser.php';</script>";
+
+    // Check if UserID is empty
+    if (empty(trim($_POST["userID"]))) {
+        $userID_err = "Please enter UserID.";
     } else {
-        $sqlAdd = "INSERT INTO Clients (UserID) VALUE ('$userID')";
-        if (mysqli_query($link, $sqlAdd)) {
-            echo "<script>alert('$userID was successfully added!');</script>";
+        $userID = trim($_POST["userID"]);
+    }
+
+    if (empty($userID_err)){
+        $sql = "SELECT UserID FROM Clients WHERE UserID = '$userID'";
+        $result = mysqli_query($link, $sql);
+        if (mysqli_num_rows($result) != 0) {
+            echo "<script>alert('$userID already exists.');window.location.href='addUser.php';</script>";
         } else {
-            echo "Oops! Something went wrong. Please try again later.";
+            $sqlAdd = "INSERT INTO Clients (UserID) VALUE ('$userID')";
+            if (mysqli_query($link, $sqlAdd)) {
+                echo "<script>alert('$userID was successfully added!');</script>";
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
         }
     }
 }
@@ -52,9 +64,12 @@ mysqli_close($link);
             <div class="col-sm-4"></div>
             <div class="col-sm-4 text-center">
                 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                    <div class="form-group">
+                    <div class="form-group <?php echo (!empty($userID_err)) ? "has-error" : ""; ?>">
                         <label>UserID</label>
                         <input type="text" id="userID" name="userID" class="form-control"/>
+                        <span class="help-block" style="color:red;">
+						    <?php echo $userID_err; ?>
+						</span>
                     </div>
                     <input type="submit" value="Add" class="btn btn-primary"/>
                     <input type="reset" class="btn btn-default"/>
