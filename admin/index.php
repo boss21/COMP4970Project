66da60343fd22ed42b6636d8af99234b7054e61d
Timothy Boss <bosst@wit.edu>
2018-04-04 23:29:18 -0400

    update

--- admin/index.php
+++ admin/index.php
@@ -80,7 +80,7 @@ include "../dbconfig.php";
                         <table class="table table-bordered">
                             <?php
                                 $sql = "
-                                SELECT Rooms.RoomID, Rooms.Room, Timeslots.Timeslot, Forms.HeadcountType, Forms.HeadcountCount, Forms.UserID, Forms.Timestamp FROM Forms LEFT JOIN Rooms ON Forms.RoomID = Rooms.RoomID LEFT JOIN Timeslots ON Forms.TimeslotID = Timeslots.TimeslotID 
+                                SELECT Rooms.RoomID, Rooms.Room, Timeslots.TimeslotID, Timeslots.Timeslot, Forms.HeadcountType, Forms.HeadcountCount, Forms.UserID, Forms.Timestamp FROM Forms LEFT JOIN Rooms ON Forms.RoomID = Rooms.RoomID LEFT JOIN Timeslots ON Forms.TimeslotID = Timeslots.TimeslotID 
                                 ORDER BY RoomID, TimeslotID, HeadcountType='End', HeadcountType='Middle', HeadcountType='Beginning'
                                 ";
 
